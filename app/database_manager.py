62a38d344020b485300909021b7ceb483b4b0ac9
O-Almeida <brunof.santos.almeida@gmail.com>
2024-09-23 19:00:55 +0100

    Create database_manager.py
    - created database_manager module

---
+++ app/database_manager.py
@@ -0,0 +1,41 @@
+# app/database_manager.py
+
+"""
+Module for managing database connections and operations.
+"""
+
+import mysql.connector
+from mysql.connector import Error
+
+class DatabaseManager:
+  """
+  Manages the database connection and operations.
+  """
+
+  def __init__(self, host:str, user:str, password:str, database:str):
+    """
+    Initializes the DatabaseManager and establishes a database connection.
+    
+    Args:
+      host (str): The database host.
+      user (str): The database user.
+      password (str): The password for the database user.
+      database (str): The name of the database.
+    """
+
+    self.connection = None
+    try:
+      self.connection = mysql.connector.connect(host=host, user=user, password=password, database=database)
+      if self.connection.is_connected():
+        print("Database connection successful.")
+    except Error as e:
+      print(f"Error connecting to database: {e}")
+
+  def close_connection(self):
+    """
+    Closes the database connection if it is open.
+    """
+    
+    if self.connection is not None and self.connection.is_connected():
+      self.connection.close()
+      print("Database connection closed.")
